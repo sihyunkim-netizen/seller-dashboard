115b9253771e7aba3712fcdd5e13a82a4850c065
sihyunkim-netizen <sihyun.kim@myrealtrip.com>
2026-04-02 19:06:25 +0900

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useSearchParams, Link } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import {
   LineChart,
   Line,
